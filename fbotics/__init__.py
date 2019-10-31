468d6054387138c5839e88f6a43d3512edb1532f
vtlya <vtlya@users.noreply.github.com>
2019-10-31 15:08:19 +0300

    Revert "change repo"
    This reverts commit f3844720e092780779e18129b15855213bc32af9.

--- fbotics/__init__.py
+++
@@ -1,9 +0,0 @@
-from fbotics.models.attachment import Attachment
-from fbotics.models.message import Message
-from fbotics.models.payloads.button_template import ButtonTemplatePayload
-from fbotics.models.recipient import Recipient
-from fbotics.models.request import Request
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
--- fbotics/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-class ANY:
-    def __init__(self, dtype):
-        self.dtype = dtype
-
-    def __eq__(self, actual):
-        return isinstance(actual, self.dtype)
