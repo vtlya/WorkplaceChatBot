b49163aee27df1c915f21338e43f672d9771c579
vtlya <vtlya@users.noreply.github.com>
2019-11-11 07:42:35 +0300

    adding break

--- app.py
+++ app.py
@@ -44,8 +44,9 @@ def receive_message():
                     recipient_id = message['sender']['id']
                     postback_body = message['postback']['payload']
                     receive_postback(recipient_id, postback_body)
-                    return 'Done'
-    return 'OK'
+                    break
+                return 'OK'
+
 
 
 def receive_postback(recipient_id, postback_body):
