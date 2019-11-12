189efcf82390bf4450d5c5f26d6c0e97ce0ab5cb
vtlya <vtlya@users.noreply.github.com>
2019-11-12 19:04:01 +0300

    updateEPT

--- app.py
+++ app.py
@@ -44,12 +44,13 @@ def receive_message():
                     recipient_id = message['sender']['id']
                     postback_body = message['postback']['payload']
                     receive_postback(recipient_id, postback_body)
-                    if message.get('text'):
-                        recipient_id = message['sender']['id']
-                        send_message(recipient_id, 'К сожалению, бот не распознает текст:( Пожалуйста, воспользуйся меню с низу как показано на картинке')
-                        send_image(recipient_id, 'https://vtlya.github.io/datastorage/mobile_bot.PNG')
-                        send_image(recipient_id, 'https://vtlya.github.io/datastorage/desctop_bot.PNG')
-                    return 'Pass'
+                    return print('recieved with postback')
+                elif message.get('text'):
+                    recipient_id = message['sender']['id']
+                    send_message(recipient_id, 'К сожалению, бот не распознает текст:( Пожалуйста, воспользуйся меню с низу как показано на картинке')
+                    send_image(recipient_id, 'https://vtlya.github.io/datastorage/mobile_bot.PNG')
+                    send_image(recipient_id, 'https://vtlya.github.io/datastorage/desctop_bot.PNG')
+                    return print('recieved with text')
 
         return 'OK'
 
